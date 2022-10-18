0875306da1622d0f6ef4950816005a6ffb681b47
Pedro Grando <grandop03@gmail.com>
2022-10-18 15:48:46 -0300

    Mudando a view pelo código

--- lession16/ViewController.swift
+++ lession16/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var rocketIdTitle: UILabel!
+    @IBOutlet weak var rocketIdImage: UIImageView!
+    @IBOutlet weak var rocketIdName: UILabel!
+    @IBOutlet weak var rocketIdOffice: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupLayout()
     }
 
-
+    func setupLayout() {
+        rocketIdTitle.text = "Programa rocket"
+        rocketIdTitle.textColor = UIColor.white
+        
+        
+        let addIdImage = UIImage(named: "downloadcirle")
+        
+        rocketIdImage.image = addIdImage
+        
+        rocketIdName.text = "Pedro Grando"
+        rocketIdOffice.text = "Estagiario"
+    }
 }
 
